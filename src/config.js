cbebf50ed6cada0ecac6555b6b8ccee421e5a73d
kasa2010 <supergrass2000@gmail.com>
2018-05-08 09:58:11 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,10 @@
+var host = 'https://876379022.supergrass.club'
+// var host = 'https://ksmqtv1c.qcloud.la'
+
+export default {
+  service: {
+      host,
+      getBuildingUrl: `${host}/weapp/building`,
+      getBuildDetail: `${host}/weapp/buildingdetail`
+  }
+}
